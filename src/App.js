a94324d6b1209fa053dd6e17e532447638eb2bfc
Tyler R. Olsen <Tyler@Tylers-MacBook-Air.local>
2019-02-25 10:12:27 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Link } from "react-router-dom"
 
-class App extends Component {
-  render() {
+class Home extends Component {
+
+state = {
+  senators: []
+}
+
+getSenators = async () => {
+await fetch('https://raw.githubusercontent.com/CivilServiceUSA/us-senate/master/us-senate/data/us-senate.json')
+    .then(results => {
+    return results.json()
+    })
+    .then(data => {
+    this.setState({senators: data})
+    });
+}
+//function does a fetch; we call that fetch 'data'; we bind that data to a state
+
+async componentWillMount () {
+await this.getSenators()
+}
+
+filterSenators= (e) => {
+let senators = this.state.senators
+senators = senators.filter(senator => {
+    return senator.name.toLowerCase().search(
+        e.target.value.toLowerCase()) !== -1
+    })
+    this.setState({senators: senators})
+}
+
+
+
+  render() {      
+    console.log(this.state.senators)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div>
+    <ul/>
+      <li>
+        <label>
+          <input type="radio" value="1" checked={this.state.priority === '1'} onChange={this.handleChange} />
+          Republican
+        </label>
+      </li>
+      <li>
+        <label>
+          <input type="radio" value="2" checked={this.state.priority === '2'} onChange={this.handleChange} />
+          Democrat
+        </label>
+      </li>
+
+  ActionLink() {
+    function handleChange(e) {
+      
+      e.preventDefault();
+      console.log('The button was clicked')
+    }
+  }
+
+      {this.state.senators.map((senator, index) => (
+          <div key={index}>
+            Name: {senator.name} 
+              <p>Party: {senator.party}</p>
+          </div>
+      ))}
+       
+        {/* <Link to="Home">Home</Link> */}
+        </div>
     );
   }
 }
-
-export default App;
+export default Home;
